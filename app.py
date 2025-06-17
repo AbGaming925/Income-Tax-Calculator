39bc9656b13b5d73adea208143ecdca80f84212d
Absolute Bittu <185889336+AbGaming925@users.noreply.github.com>
2025-06-17 17:04:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def index():
         except:
             result = {"error": "Invalid input"}
 
-    return render_template("index.html", result=result)
+    return render_template("template/index.html", result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
